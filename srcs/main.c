aae21c2986db884f17e4bde46c1e64d9b85855c9
mint <officialscientist42@gmail.com>
2019-11-03 18:38:35 -0800

    E_NO_ARGS -> E_USAGE

--- srcs/main.c
+++ srcs/main.c
@@ -7,7 +7,7 @@ int				main(int argc, char **argv)
 	struct s_term	term;
 
 	if (argc < 2)
-		return (print_error(E_NO_ARGS));
+		return (print_error(E_USAGE));
 	if (setup_terminal(&term) == ERROR)
 		return (ERROR);
 	if (select(argc, argv, &term))
