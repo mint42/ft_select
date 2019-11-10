348bde3db2314d55f9f2b91a673314e68736aa81
Ari <arireedy42@gmail.com>
2019-11-09 22:07:08 -0800

    dividing repetitive code into function

--- srcs/screen.c
+++ srcs/screen.c
@@ -6,13 +6,13 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 17:21:31 by rreedy            #+#    #+#             */
-/*   Updated: 2019/11/09 19:48:48 by rreedy           ###   ########.fr       */
+/*   Updated: 2019/11/09 21:56:16 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "errors.h"
 #include "ft_put.h"
-#include <ncurses.h>
+#include <curses.h>
 #include <term.h>
 #include <unistd.h>
 
@@ -21,36 +21,31 @@ static int	ft_putnbr_stdin(int n)
 	return (write(STDIN_FILENO, &n, 1));
 }
 
-int			setup_screen(void)
+static int	put_tc(char *id)
 {
 	char	*termcap_string;
 
-	termcap_string = tgetstr("ti", 0);
-	if (!termcap_string)
-		return (set_error(E_TGETSTR));
-	tputs(termcap_string, 1, ft_putnbr_stdin);
-	termcap_string = tgetstr("vi", 0);
-	if (!termcap_string)
-		return (set_error(E_TGETSTR));
-	tputs(termcap_string, 1, ft_putnbr_stdin);
-	termcap_string = tgetstr("cl", 0);
+	termcap_string = tgetstr(id, 0);
 	if (!termcap_string)
 		return (set_error(E_TGETSTR));
 	tputs(termcap_string, 1, ft_putnbr_stdin);
 	return (SUCCESS);
 }
 
+int			setup_screen(void)
+{
+	if (put_tc("ti") == ERROR)
+		return (ERROR);
+	if (put_tc("vi") == ERROR)
+		return (ERROR);
+	return (SUCCESS);
+}
+
 int			reset_screen(void)
 {
-	char	*termcap_string;
-
-	termcap_string = tgetstr("te", 0);
-	if (!termcap_string)
-		return (set_error(E_TGETSTR));
-	tputs(termcap_string, 1, ft_putnbr_stdin);
-	termcap_string = tgetstr("ve", 0);
-	if (!termcap_string)
-		return (set_error(E_TGETSTR));
-	tputs(termcap_string, 1, ft_putnbr_stdin);
+	if (put_tc("te") == ERROR)
+		return (ERROR);
+	if (put_tc("ve") == ERROR)
+		return (ERROR);
 	return (SUCCESS);
 }
