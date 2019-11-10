b5b23c8b9fed50b93c9323f9bbe11460828a3af1
Ari <arireedy42@gmail.com>
2019-11-10 07:39:34 -0800

    fix leak

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 04:46:12 by rreedy            #+#    #+#             */
-/*   Updated: 2019/11/10 07:05:12 by rreedy           ###   ########.fr       */
+/*   Updated: 2019/11/10 07:36:33 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,23 @@
 #include "screen.h"
 #include "struct_info.h"
 #include "struct_term.h"
+#include "ft_mem.h"
 #include <signal.h>
 #include <stdlib.h>
 
 static void		restore_and_exit(int sig)
 {
 	struct s_term	*term;
+	struct s_info	*info;
 
 	(void)sig;
 	hold_term(&term, GET_TERM);
+	hold_info(&info, GET_INFO);
 	if (reset_screen(term) == ERROR)
 		exit(ERROR);
 	if (reset_term(term) == ERROR)
 		exit(ERROR);
+	ft_memdel((void **)&(info->args));
 	print_error();
 	exit(ERROR);
 }
