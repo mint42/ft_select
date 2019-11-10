6ea57efd64ac0a218a7d936ed74c17c92197c3b0
Ari <arireedy42@gmail.com>
2019-11-10 05:16:37 -0800

    reduce writing with escape sequence variable

--- srcs/select.c
+++ srcs/select.c
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 04:43:52 by rreedy            #+#    #+#             */
-/*   Updated: 2019/11/10 04:55:17 by rreedy           ###   ########.fr       */
+/*   Updated: 2019/11/10 05:14:16 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,25 @@ void	print_string(struct s_arg *arg, uint32_t coord, struct s_info *info)
 {
 	uint32_t	x_pos;
 	uint32_t	y_pos;
+	char		*esc_seq;
 
+	esc_seq = 0;
 	x_pos = (coord % info->n_columns) * (info->column_width + COLUMN_PADDING);
 	y_pos = coord / info->n_columns;
 	tc_move_cur(x_pos, y_pos);
-	if (arg->status == SELECTED)
-		ft_printfd(STDIN_FILENO, "\e[7m");
-	if (coord == info->cursor_coord)
-		ft_printfd(STDIN_FILENO, "\e[4m");
-	ft_printfd(STDIN_FILENO, "%-*s\e[0m", COLUMN_PADDING, arg->name);
+	if (arg->status == SELECTED && coord == info->cursor_coord)
+		esc_seq = "\e[7;4m";
+	else if (arg->status == SELECTED)
+		esc_seq = "\e[7m";
+	else if (coord == info->cursor_coord)
+		esc_seq = "\e[4m";
+	if (esc_seq)
+	{
+		ft_printfd(STDIN_FILENO, "%s%-*s\e[0m", esc_seq, COLUMN_PADDING,
+			arg->name);
+	}
+	else
+		ft_printfd(STDIN_FILENO, "%-*s", COLUMN_PADDING, arg->name);
 }
 
 void	print_screen(struct s_info *info)
