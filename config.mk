5b9c6d910a89721bc04fed2976e774c248bb7131
Ari <arireedy42@gmail.com>
2020-01-09 05:43:05 -0800

    syntax update

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@
 #    By: rreedy <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/22 09:39:36 by rreedy            #+#    #+#              #
-#    Updated: 2019/11/01 01:55:43 by rreedy           ###   ########.fr        #
+#    Updated: 2020/01/09 05:42:25 by rreedy           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,5 +30,7 @@ CFLAGS := -g
 CFLAGS += -Wall
 CFLAGS += -Wextra
 CFLAGS += -Werror
-CFLAGS += $(foreach include,$(INCLUDE_DIRS),-I$(include)) $(foreach lib_include,$(LIB_INCLUDE_DIRS),-I$(lib_include))
-LDFLAGS := $(foreach local_lib,$(LIB_DIRS),-L$(local_lib)) $(foreach lib,$(LIBS),-l$(lib))
+CFLAGS += $(foreach include,$(INCLUDE_DIRS),-I$(include))
+CFLAGS += $(foreach lib_include,$(LIB_INCLUDE_DIRS),-I$(lib_include))
+LDFLAGS := $(foreach local_lib_dirs,$(LIB_DIRS),-L$(local_lib_dirs))
+LDFLAGS += $(foreach lib,$(LIBS),-l$(lib))
